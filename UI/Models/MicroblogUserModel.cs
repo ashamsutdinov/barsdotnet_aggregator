974c5107e0bf4c91a41e25f59d230d8a07ac8b9a
Kamil Zakiev <zakiev-kamil@mail.ru>
2015-06-07 23:45:30 +0300

    full my part

---
+++ UI/Models/MicroblogUserModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UI.Models
+{
+    class DescendedStringComparer 
+        : IComparer<String>
+    {
+        public int Compare(String x, String y)
+        {
+            // use the default comparer to do the original comparison for strings
+            int ascendingResult = Comparer<String>.Default.Compare(x, y);
+
+            // turn the result around
+            return 0 - ascendingResult;
+        }
+    }
+    public class MicroblogUserModel
+    {
+        public string Name { get; set; }
+
+        public SortedList<String, Tweet> feed = new SortedList<string,Tweet>(new DescendedStringComparer());
+    }
+}
